eb3fde48edc62dfbf416d50d189f162f38e110fa
Shajk0 <108530357+Shajk0@users.noreply.github.com>
2025-05-13 14:56:49 +0200

    changed padding

--- lib/src/widgets/countries_search_list_widget.dart
+++ lib/src/widgets/countries_search_list_widget.dart
@@ -74,35 +74,35 @@ class _CountrySearchListWidgetState extends State<CountrySearchListWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Padding(
-            padding: widget.searchBoxPadding ??
-                const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-            child: TextFormField(
-              key: Key(TestHelper.CountrySearchInputKeyValue),
-              decoration: getSearchBoxDecoration(),
-              controller: _searchController,
-              autofocus: widget.autoFocus,
-              cursorColor: Colors.black,
-              onChanged: (value) {
-                final String value = _searchController.text.trim();
-                return setState(
-                  () => filteredCountries = Utils.filterCountries(
-                    countries: widget.countries,
-                    locale: widget.locale,
-                    value: value,
-                  ),
-                );
-              },
-            ),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Padding(
+          padding: widget.searchBoxPadding ??
+              const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+          child: TextFormField(
+            key: Key(TestHelper.CountrySearchInputKeyValue),
+            decoration: getSearchBoxDecoration(),
+            controller: _searchController,
+            autofocus: widget.autoFocus,
+            cursorColor: Colors.black,
+            onChanged: (value) {
+              final String value = _searchController.text.trim();
+              return setState(
+                () => filteredCountries = Utils.filterCountries(
+                  countries: widget.countries,
+                  locale: widget.locale,
+                  value: value,
+                ),
+              );
+            },
           ),
-          Expanded(
-            child: SingleChildScrollView(
-              controller: widget.scrollController,
+        ),
+        Expanded(
+          child: SingleChildScrollView(
+            controller: widget.scrollController,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
               child: Column(
                 children: [
                   if (favoriteCountries != null &&
@@ -145,8 +145,8 @@ class _CountrySearchListWidgetState extends State<CountrySearchListWidget> {
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
